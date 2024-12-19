48a0c81ce8d020bd88572af03cde3c9117c7005f
kraanzu <kraanzu@gmail.com>
2024-12-19 21:11:06 +0530

    refactor: split update hooks

--- dooit/api/hooks/update_hooks.py
+++ dooit/api/hooks/update_hooks.py
@@ -4,7 +4,10 @@ from ..todo import Todo
 
 
 @event.listens_for(Todo, "before_update")
-def update_pending_status_child(_, connection, target: Todo):
+def update_children_to_pending(_, connection, target: Todo):
+    if not target.pending:
+        return
+        
     to_update = []
 
     def update_children(todo: Todo):
@@ -12,25 +15,54 @@ def update_pending_status_child(_, connection, target: Todo):
             to_update.append(child.id)
             update_children(child)
 
-    # mark all siblings as completed
     update_children(target)
-    query = update(Todo).where(Todo.id.in_(to_update)).values(pending=target.pending)
+    query = update(Todo).where(Todo.id.in_(to_update)).values(pending=True)
     connection.execute(query)
 
 
 @event.listens_for(Todo, "before_update")
-def update_pending_status_parent(mapper, connection, target: Todo):
-    to_update_as_pending = []
+def update_children_to_completed(_, connection, target: Todo):
+    if target.pending:
+        return
+        
+    to_update = []
+
+    def update_children(todo: Todo):
+        for child in todo.todos:
+            to_update.append(child.id)
+            update_children(child)
+
+    update_children(target)
+    query = update(Todo).where(Todo.id.in_(to_update)).values(pending=False)
+    connection.execute(query)
+
+
+@event.listens_for(Todo, "before_update")
+def update_parent_to_pending(mapper, connection, target: Todo):
+    if not target.pending:
+        return
+        
+    to_update = []
 
     def update_parent_pending(todo: Todo):
         parent = todo.parent_todo
         if not parent:
             return
 
-        to_update_as_pending.append(parent.id)
+        to_update.append(parent.id)
         update_parent_pending(parent)
 
-    to_update_as_completed = []
+    update_parent_pending(target)
+    query = update(Todo).where(Todo.id.in_(to_update)).values(pending=True)
+    connection.execute(query)
+
+
+@event.listens_for(Todo, "before_update") 
+def update_parent_to_completed(mapper, connection, target: Todo):
+    if target.pending:
+        return
+        
+    to_update = []
 
     def update_parent_completed(todo: Todo):
         parent = todo.parent_todo
@@ -40,22 +72,11 @@ def update_pending_status_parent(mapper, connection, target: Todo):
         all_sibling_completed = all([not sibling.pending for sibling in parent.todos])
         if all_sibling_completed:
             parent.pending = False
-            to_update_as_completed.append(parent.id)
+            to_update.append(parent.id)
             update_parent_completed(parent)
 
-    if target.pending:
-        update_parent_pending(target)
-        query = (
-            update(Todo).where(Todo.id.in_(to_update_as_pending)).values(pending=True)
-        )
-    else:
-        update_parent_completed(target)
-        query = (
-            update(Todo)
-            .where(Todo.id.in_(to_update_as_completed))
-            .values(pending=False)
-        )
-
+    update_parent_completed(target)
+    query = update(Todo).where(Todo.id.in_(to_update)).values(pending=False)
     connection.execute(query)
 
 
