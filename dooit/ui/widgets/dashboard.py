0d3ca5d3526f72ff9e30e16e92f663e7e62ce44f
kraanzu <murlidhar.tawari@fulfil.io>
2025-04-12 11:08:33 +0530

    Fix height on dashboard widget

--- dooit/ui/widgets/dashboard.py
+++ dooit/ui/widgets/dashboard.py
@@ -1,15 +1,15 @@
 from rich.console import RenderableType
 from textual.app import ComposeResult
 from textual.reactive import reactive
-from textual.widget import Widget
-from textual.widgets import Label
+from textual.widgets import Label, Static
 
 
-class Dashboard(Widget):
+class Dashboard(Static):
     DEFAULT_CSS = """
     Dashboard {
         align: center middle;
         content-align: center middle;
+        height: 1fr;
     }
 
     Dashboard > Label {
